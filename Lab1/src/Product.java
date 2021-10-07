ed75ea7514dd0ab036682b9396cc6a79c49979d2
GhermanCristian <gherman_cristi10@yahoo.com>
2021-10-07 22:43:52 +0300

    Lab 1 Created the Product class

---
+++ Lab1/src/Product.java
@@ -0,0 +1,25 @@
+public class Product {
+    private final String name;
+    private final double price;
+
+    public Product(String name, double price) {
+        this.name = name;
+        this.price = price;
+    }
+
+    public String getName() {
+        return this.name;
+    }
+
+    public double getPrice() {
+        return this.price;
+    }
+
+    @Override
+    public String toString() {
+        return "Product{" +
+                "name='" + name + '\'' +
+                ", price=" + price +
+                '}';
+    }
+}
