772c39ab57eb0e66966e977187276b66e8ed74a0
GhermanCristian <gherman_cristi10@yahoo.com>
2021-11-28 23:41:10 +0200

    Lab 5 Added a timer for each multiplication operation

--- Lab5/src/Main.java
+++ Lab5/src/Main.java
@@ -10,9 +10,14 @@ public class Main {
         Polynomial x = new Polynomial(Arrays.asList(1, 2, -2, 6, 3)); // 3x^4 + 6x^3 -2x^2 + 2x + 1
         Polynomial y = new Polynomial(Arrays.asList(4, -2, 3, 0, 7, 2, 1)); // x^6 + 2x^5 + 7x^4 + 3x^2 -2x + 4
         // 4X ^ 0 + 6X ^ 1 + -9X ^ 2 + 34X ^ 3 + 1X ^ 4 + 28X ^ 5 + 40X ^ 7 + 31X ^ 8 + 12X ^ 9 + 3X ^ 10
+        long time = System.currentTimeMillis();
         System.out.println(Multiplication.regularSequentialMultiplication(x, y));
+        System.out.println(System.currentTimeMillis() - time);
         System.out.println(Multiplication.regularParallelMultiplication(x, y));
+        System.out.println(System.currentTimeMillis() - time);
         System.out.println(Multiplication.karatsubaSequentialMultiplication(x, y));
+        System.out.println(System.currentTimeMillis() - time);
         System.out.println(Multiplication.karatsubaParallelMultiplication(x, y));
+        System.out.println(System.currentTimeMillis() - time);
     }
 }
