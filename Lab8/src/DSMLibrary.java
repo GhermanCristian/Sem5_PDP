5f0624cf9631af1db0b7caeac2733b531623a8d1
GhermanCristian <gherman_cristi10@yahoo.com>
2021-12-14 22:45:07 +0200

    Lab 8 Added the DSMLibrary class; first half-working version

---
+++ Lab8/src/DSMLibrary.java
@@ -0,0 +1,77 @@
+import mpi.MPI;
+
+import java.util.HashMap;
+import java.util.HashSet;
+import java.util.Map;
+import java.util.Set;
+import java.util.concurrent.ConcurrentHashMap;
+import java.util.concurrent.locks.Lock;
+import java.util.concurrent.locks.ReentrantLock;
+
+public class DSMLibrary {
+    private final Map<String, Set<Integer>> subscribers;
+    public static final Lock lock = new ReentrantLock();
+    private final Map<String, Integer> variables;
+
+    public DSMLibrary() {
+        this.subscribers = new ConcurrentHashMap<>();
+        this.variables = new HashMap<>();
+        this.variables.put("a", 1);
+        this.variables.put("b", 2);
+        this.variables.put("c", 3);
+        this.variables.put("d", 4);
+        this.variables.keySet().forEach(variable -> this.subscribers.put(variable, new HashSet<>()));
+    }
+
+    private void writeVariableAndSendMessage(String variableName, int newValue) {
+        this.variables.put(variableName, newValue);
+        this.sendMessageToSubscribers(variableName, new WriteMessage(variableName, newValue));
+    }
+
+    public void writeVariable(String variableName, int newValue) {
+        lock.lock();
+        this.writeVariableAndSendMessage(variableName, newValue);
+        lock.unlock();
+    }
+
+    public void compareAndExchange(String variableName, int oldValue, int newValue) {
+        lock.lock();
+        if (this.variables.get(variableName) == oldValue) {
+            this.writeVariableAndSendMessage(variableName, newValue);
+        }
+        lock.unlock();
+    }
+
+    public void addNewSubscriptionToVariable(String variableName, int processRank) {
+        this.subscribers.get(variableName).add(processRank);
+    }
+
+    public void subscribeCurrentProcessToVariable(String variableName) {
+        this.subscribers.get(variableName).add(MPI.COMM_WORLD.Rank());
+        this.sendMessageToAllProcesses(new SubscribeMessage(variableName, MPI.COMM_WORLD.Rank()));
+    }
+
+    public void sendMessageToSubscribers(String variableName, BaseMessage message) {
+        this.subscribers.get(variableName).forEach(subscriberRank -> MPI.COMM_WORLD.Send(new Object[]{message}, 0, 1, MPI.OBJECT, subscriberRank, 0));
+    }
+
+    private void sendMessageToAllProcesses(BaseMessage message) {
+        for (int i = 0; i < MPI.COMM_WORLD.Size(); i++) {
+            if (MPI.COMM_WORLD.Rank() == i && !(message instanceof CloseMessage))
+                continue;
+            MPI.COMM_WORLD.Send(new Object[]{message}, 0, 1, MPI.OBJECT, i, 0);
+        }
+    }
+
+    public void close() {
+        this.sendMessageToAllProcesses(new CloseMessage());
+    }
+
+    @Override
+    public String toString() {
+        return "DSMLibrary{" +
+                "subscribers=" + this.subscribers +
+                ", variables=" + this.variables +
+                '}';
+    }
+}
--- Lab8/src/Main.java
+++ Lab8/src/Main.java
@@ -1,5 +1,45 @@
-public class Main {
-    public static void main(String[] args) {
+import mpi.MPI;
 
+import java.util.HashMap;
+import java.util.Map;
+import java.util.function.Consumer;
+
+public class Main {
+    public static void process0Behaviour(DSMLibrary dsmLibrary) {
+        dsmLibrary.subscribeCurrentProcessToVariable("a");
+        dsmLibrary.subscribeCurrentProcessToVariable("b");
+        dsmLibrary.subscribeCurrentProcessToVariable("c");
+        dsmLibrary.compareAndExchange("a", 1, 69);
+        dsmLibrary.compareAndExchange("d", 4, 437);
+        dsmLibrary.compareAndExchange("b", 901, 2307);
+        dsmLibrary.close();
+    }
+
+    public static void process1Behaviour(DSMLibrary dsmLibrary) {
+        dsmLibrary.subscribeCurrentProcessToVariable("a");
+        dsmLibrary.subscribeCurrentProcessToVariable("d");
+    }
+
+    public static void process2Behaviour(DSMLibrary dsmLibrary) {
+        dsmLibrary.subscribeCurrentProcessToVariable("b");
+        dsmLibrary.compareAndExchange("b", 1, 901);
+    }
+
+    public static void main(String[] args) throws InterruptedException {
+        MPI.Init(args);
+        int rank = MPI.COMM_WORLD.Rank();
+        System.out.println("Current rank = " + rank);
+        DSMLibrary dsmLibrary = new DSMLibrary();
+        Map<Integer, Consumer<DSMLibrary>> behaviours = new HashMap<>();
+        behaviours.put(0, Main::process0Behaviour);
+        behaviours.put(1, Main::process1Behaviour);
+        behaviours.put(2, Main::process2Behaviour);
+
+        Thread thread = new Thread(new Subscriber(dsmLibrary));
+        thread.start();
+        behaviours.get(rank).accept(dsmLibrary);
+        thread.join();
+
+        MPI.Finalize();
     }
 }
