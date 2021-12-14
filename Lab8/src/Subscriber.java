9c1c026a665f677d0ac17f2e6b03561050f961e0
GhermanCristian <gherman_cristi10@yahoo.com>
2021-12-14 22:35:01 +0200

    Lab 8 Added the Subscriber class

---
+++ Lab8/src/Subscriber.java
@@ -0,0 +1,39 @@
+import mpi.MPI;
+
+public class Subscriber implements Runnable {
+    private final DSMLibrary dsmLibrary;
+
+    public Subscriber(DSMLibrary dsmLibrary) {
+        this.dsmLibrary = dsmLibrary;
+    }
+
+    @Override
+    public void run() {
+        while (true) {
+            System.out.println("Rank " + MPI.COMM_WORLD.Rank() + " waiting..");
+            Object[] messageObject = new Object[1];
+
+            MPI.COMM_WORLD.Recv(messageObject, 0, 1, MPI.OBJECT, MPI.ANY_SOURCE, MPI.ANY_TAG);
+            BaseMessage message = (BaseMessage) messageObject[0];
+
+            if (message instanceof CloseMessage){
+                System.out.println("Rank " + MPI.COMM_WORLD.Rank() + " stopped listening...");
+                return;
+            }
+            else if (message instanceof SubscribeMessage) {
+                SubscribeMessage subscribeMessage = (SubscribeMessage) message;
+                System.out.println("Subscribe message received");
+                System.out.println("Rank " + MPI.COMM_WORLD.Rank() + " received: rank " + subscribeMessage.rank + " subscribes to " + subscribeMessage.variableName);
+                this.dsmLibrary.addNewSubscriptionToVariable(subscribeMessage.variableName, subscribeMessage.rank);
+            }
+            else if (message instanceof WriteMessage) {
+                WriteMessage writeMessage = (WriteMessage) message;
+                System.out.println("Update message received");
+                System.out.println("Rank " + MPI.COMM_WORLD.Rank() + " received:" + writeMessage.variableName + "->" + writeMessage.newValue);
+                this.dsmLibrary.writeVariable(writeMessage.variableName, writeMessage.newValue);
+            }
+
+            System.out.println(this.dsmLibrary);
+        }
+    }
+}
